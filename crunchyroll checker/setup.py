5b91688f424e1e610ecc6cdc639e95dc74b8c930
Majinooo <futurebillionaire.nomoretears@gmail.com>
2026-06-27 23:38:37 +0200

    Delete crunchyroll checker directory

--- crunchyroll checker/setup.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-
-packages = ['requests', 'colorama', 'pyperclip', 'pycryptodome']
-for p in packages:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", p])
-
-subprocess.run([sys.executable, "dist/check.py"])
